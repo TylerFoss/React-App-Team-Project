bdd5589ae14c2261c95378d82d8d89e33e90dc3a
Tyler Foss <tylerscottfoss@gmail.com>
2017-02-16 19:08:24 -0700

    updated todo list

---
+++ src/components/TodoApp.js
@@ -0,0 +1,56 @@
+import React from "react";
+import List from "./List";
+import TodoForm from "./TodoForm";
+
+class TodoApp extends React.Component{
+  constructor(props) {
+      super(props);
+      this.addTodoItem = this.addTodoItem.bind(this);
+      this.onTodoClick = this.onTodoClick.bind(this);
+      this.state = { items: [], id: 0 }
+    }
+
+    onTodoClick(id) {
+      let items = this.state.items.map( item => {
+        if ( item.id === id )  {
+          return {
+            ...item,
+            complete: !item.complete
+          }
+        }
+
+        return item;
+      });
+
+      this.setState({ items });
+    }
+
+  //..
+
+  render() {
+    //..
+    <List items={this.state.items} onTodoClick={this.onTodoClick} />
+  }
+
+  addTodoItem(name){
+    let id = ++this.state.id;
+    this.setState({
+      items: [
+        {name, id, complete: false},
+        ...this.state.items
+      ],
+      id
+    })
+  }
+
+  render(){
+    return(
+      <div>
+      <TodoForm addTodoItem={this.addTodoItem} />
+      <List items={this.state.items} />
+      </div>
+    )
+  }
+}
+
+export default TodoApp;
